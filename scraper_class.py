4fd77cad1e4a79486e74c987f9e86a9de3c122a8
Jason <xiaohemc2001@gmail.com>
2022-07-23 20:30:43 +0100

    Adding button shortcuts

---
+++ scraper_class.py
@@ -0,0 +1,44 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.myprotein.com/")
+
+exit_signup_button = driver.find_element(by=By.XPATH, value='//*[@id="home"]/div[4]/div/div[2]/button')
+exit_signup_button.click()
+
+
+accept_cookies_button = driver.find_element(by=By.XPATH, value='//*[@id="home"]/div[1]/div/div/div[2]/button')
+accept_cookies_button.click()
+
+def open_protein_page():
+        
+    protein_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[1]')
+    protein_button.click()
+
+def open_bars_and_snacks_page():
+    
+    bars_and_snacks_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[2]')
+    bars_and_snacks_button.click()
+
+def open_clothing_page():
+    
+    clothing_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[3]')
+    clothing_button.click()
+
+def open_vitamins_page():
+    
+    vitamins_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[4]')
+    vitamins_button.click()
+
+def open_vegans_page():
+    
+    vegans_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[5]')
+    vegans_button.click()
+
+def open_creatine_page():
+    creatine_button = driver.find_element(by=By.XPATH, value= '//*[@id="mainContent"]/div[2]/a[5]')
+    creatine_button.click()
+
+driver.quit()
