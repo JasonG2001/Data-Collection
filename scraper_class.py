6daac04834dd5df0ccc5cc0f5c0f7b6d74050c01
Jason <xiaohemc2001@gmail.com>
2022-07-25 15:31:45 +0100

    Adding scrape link

--- scraper_class.py
+++ scraper_class.py
@@ -1,14 +1,17 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 from web_navigator import WebNavigator
 
 class Scraper:
 
     def scrape_links(self):
-        item_container = self.driver.find_element(by=By.XPATH, value='')
-        item_list = item_container.find_element(by=By.XPATH, value='./div')
+        item_container = self.driver.find_element(by=By.XPATH, value='.//*[@id="mainContent"]/div[3]/ul') # Test for creatine page
+        item_list = item_container.find_element(by=By.XPATH, value='./li')
         link_list = []
+
+        # productListProducts_product 
 
         for item in item_list:
             a_tag = item.driver.find_element(by=By.XPATH, value='a')
@@ -24,4 +27,7 @@ class Scraper:
     def scrape_description(self):
         pass
 
+scraper = Scraper()
+
+scraper.scrape_links()
 
