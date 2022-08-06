1fbe5a566eb9ad8107814e7b3c5f73ad8eeb4d8f
Jason <xiaohemc2001@gmail.com>
2022-08-06 22:41:31 +0100

    Make setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name = "Data-collection pipeline",
-    description = "Scraper which allows you to scrape the information from the myprotein website",
-    url = "https://github.com/JasonG2001/Data-Collection",
 
-    author = "Jason Guan",
-    license = "MIT",
-    packages = find_packages()
-    install_requires = ["selenium", "time", "web_navigator", "urllib.request", "uuid", "os", "json"],
+    name="project",
+    description="Scraper which allows you to scrape the information from the myprotein website",
+    version="1.0.0",
+    url="https://github.com/JasonG2001/Data-Collection",
+
+    author="Jason Guan",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["selenium"]
 
 )
